359f747840c4fd7820edb0adcec537586224e55d
rahmoniy <islomrahmoniy@gmail.com>
2024-05-07 12:08:45 +0500

    first commit

--- android/app/src/main/java/com/SOFF_marketplace/MainActivity.java
+++ android/app/src/main/java/com/SOFF_marketplace/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SOFF_marketplace";
+    return "DOCTOR";
   }
 
   /**
--- ios/SOFF_marketplaceTests/bis_salesTests.m
+++ ios/SOFF_marketplaceTests/bis_salesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SOFF_marketplaceTests : XCTestCase
+@interface DOCTORTests : XCTestCase
 
 @end
 
-@implementation SOFF_marketplaceTests
+@implementation DOCTORTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
